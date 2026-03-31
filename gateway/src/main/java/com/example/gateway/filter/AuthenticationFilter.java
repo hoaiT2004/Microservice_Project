a788e84209150be0e656f15f67bbfb664d22f5cf
Nguyen Xuan Hoa <nguyenxuanhoa8b@gmail.com>
2026-03-31 22:53:13 +0700

    [fix] Fix route in gateway

---
+++ gateway/src/main/java/com/example/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.example.gateway.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+                // Check if header contains token
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    return onError(exchange, "Missing authorization header", HttpStatus.UNAUTHORIZED);
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                // Call auth-service to validate token
+                return webClientBuilder.build()
+                        .get()
+                        .uri("http://auth-service/api/v1/auth/validate?token=" + authHeader)
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .map(response -> exchange)
+                        .flatMap(chain::filter)
+                        .onErrorResume(e -> onError(exchange, "Unauthorized access to application", HttpStatus.UNAUTHORIZED));
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        exchange.getResponse().setStatusCode(httpStatus);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {
+        // Put configuration properties here
+    }
+}
